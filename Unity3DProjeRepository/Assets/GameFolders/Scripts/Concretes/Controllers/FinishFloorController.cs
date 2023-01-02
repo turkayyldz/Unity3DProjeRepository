8bd0dc7f02a6ff4bd7032566dd09f71710c34236
turkayyldz <79602292+turkayyldz@users.noreply.github.com>
2023-01-03 02:32:17 +0300

    FinisFloorController
    FinishFloorController eklendi gameobject ustune eklendi, oyun bitince çalışır.

---
+++ Unity3DProjeRepository/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity3DProjeRepository.Controllers
+{
+    public class FinishFloorController : MonoBehaviour
+    {
+        [SerializeField] GameObject _finishFireWork;
+        [SerializeField] GameObject _finisLight;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+            if (player == null) return;
+            // üstten aþþaðýya dokunmasýný saðlýyor.
+            if (other.GetContact(0).normal.y==-1)
+            {
+                _finishFireWork.gameObject.SetActive(true);
+                _finisLight.gameObject.SetActive(true);
+
+            }
+            else
+            {
+                //Game oVER
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+           
+        }
+    }
+}
+
