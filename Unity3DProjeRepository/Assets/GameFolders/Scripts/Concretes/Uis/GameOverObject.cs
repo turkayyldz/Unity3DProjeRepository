9aa70981d3f2cc28c6b9401a12b60c7b92d0a6a6
turkayyldz <79602292+turkayyldz@users.noreply.github.com>
2023-01-05 22:24:32 +0300

    game over
    game over uı eklendi

---
+++ Unity3DProjeRepository/Assets/GameFolders/Scripts/Concretes/Uis/GameOverObject.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity3DProjeRepository.Managers;
+using System;
+
+namespace Unity3DProjeRepository.Uis
+{
+    public class GameOverObject : MonoBehaviour
+    {
+        [SerializeField] GameObject _gameOverPanel;
+
+        private void Awake()
+        {
+            if (_gameOverPanel)
+            {
+                _gameOverPanel.SetActive(false);
+            }
+        }
+        private void OnEnable()
+        {
+            GameManager.Instance.OnGameOver += HandleOnGameOver;
+          
+        }
+
+        
+
+        private void OnDisable()
+        {
+            GameManager.Instance.OnGameOver -= HandleOnGameOver;
+            
+        }
+        private void HandleOnGameOver()
+        {
+            if (_gameOverPanel.activeSelf)
+            {
+                _gameOverPanel.SetActive(true);
+            }
+            
+        }
+    }
+
+
+}
+
