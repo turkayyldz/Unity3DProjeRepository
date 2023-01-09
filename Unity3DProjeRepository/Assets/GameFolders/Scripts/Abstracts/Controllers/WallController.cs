82349819862f296471897998fdcc115b0c51db83
turkayyldz <79602292+turkayyldz@users.noreply.github.com>
2023-01-10 01:40:03 +0300

    wall Controller

--- Unity3DProjeRepository/Assets/GameFolders/Scripts/Abstracts/Controllers/WallController.cs
+++ Unity3DProjeRepository/Assets/GameFolders/Scripts/Abstracts/Controllers/WallController.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Unity3DProjeRepository.Managers;
 
 
 namespace Unity3DProjeRepository.Controllers
 {
-    public class WallController : MonoBehaviour
+    public abstract class WallController : MonoBehaviour
     {
         private void OnCollisionEnter(Collision other)
         {
---
+++ Unity3DProjeRepository/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity3DProjeRepository.Controllers
+{
+    public class MoverWallController : MonoBehaviour
+    {
+
+    }
+}
+
