70bcc7a7370d21395bfdcaf7b8337b42dc6c9483
turkayyldz <79602292+turkayyldz@users.noreply.github.com>
2023-01-10 23:46:43 +0300

    Level Sistemi
    Level 2 ve Level 3 yapıldı. Engeller yapildi.

--- Unity3DProjeRepository/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
+++ Unity3DProjeRepository/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
@@ -8,19 +8,28 @@ namespace Unity3DProjeRepository.Controllers
     public class MoverWallController : WallController
     {
         [SerializeField] Vector3 _direction;
-        [Range(0f,1f)]
-        [SerializeField] float _factor;
+       
+        [SerializeField] float _speed=1f;
+       
+
 
         Vector3 _startPosition;
-
+        float _factor;
+        private const float FULL_CIRCLE = Mathf.PI * 2f;
         private void Awake()
         {
             _startPosition = transform.position;
         }
         private void Update()
         {
+            float cycle = Time.time / _speed;
+            float sinWave =Mathf.Sin( cycle * FULL_CIRCLE);
+
+            // _factor = Mathf.Abs(sinWave);
+            _factor = sinWave / 2f + 0.5f;
             Vector3 offset = _direction * _factor;
             transform.position = offset + _startPosition;
+            
         }
     }
 }
